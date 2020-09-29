270ca39e57bba7e75595badaadb29cf206a7e923
jiana2107 <71509223+jiana2107@users.noreply.github.com>
2020-09-29 11:02:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function setup() {
   car.velocityX=speed;
 
   wall=createSprite(1500,200,60,height/2);
-  wall.shapeColor = "black";
+  wall.shapeColor = color(80,80,80);
 }
 
 function draw() {
@@ -26,13 +26,13 @@ function draw() {
      var deformation=0.5*weight*speed*speed/22509;
 
     if(deformation>180){
-      car.shapeColor=(255,0,0)}
+      car.shapeColor=color(255,0,0)}
 
     if(deformation<180 && deformation>100){
-       car.shapeColor=(230,230,0)}
+       car.shapeColor=color(230,230,0)}
 
     if(deformation<180){
-      car.shapeColor=(0,255,0)}
+      car.shapeColor=color(0,255,0)}
   }
   
 
