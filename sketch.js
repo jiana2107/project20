5d67b93c8f84cc03ae8aa9413ccd9fd9fd236705
jiana2107 <71509223+jiana2107@users.noreply.github.com>
2020-10-02 13:19:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,13 +27,13 @@ function draw() {
      deformation=0.5*weight*speed*speed/22500;
 
     if(deformation>180){
-      car.shapeColor=color(0,255,0)}
+      car.shapeColor=color(255,0,0)}
 
     if(deformation<180 && deformation>100){
        car.shapeColor=color(230,230,0)}
 
     if(deformation<100){
-      car.shapeColor=color(255,0,0)}
+      car.shapeColor=color(0,255,0)}
   }
   
 
